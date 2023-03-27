31a2c6c35597229e7ead42498e51b5221bf37587
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-03-27 08:50:28 +0200

    Task 2 bugs

--- lab3/utils.cpp
+++ lab3/utils.cpp
@@ -15,11 +15,11 @@ void on_mouse(int event, int x, int y, int f, void* userdata){
 
     Mat img = *(Mat*) userdata;
     
-    // Task 2
+    // ---------------- Task 2 ----------------
     int b = img.at<Vec3b>(x,y)[0];
     int g = img.at<Vec3b>(x,y)[1];
     int r = img.at<Vec3b>(x,y)[2];
 
-    std::cout << "[Task2] BGR:" << std::to_string(b) << ", " << std::to_string(g) << ", " std::to_string(r) << "\n";
+    std::cout << "[Task2] BGR: " << std::to_string(b) << ", " << std::to_string(g) << ", " << std::to_string(r) << "\n";
 
 }
--- lab3/utils.h
+++ lab3/utils.h
@@ -3,3 +3,5 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+
+void on_mouse(int event, int x, int y, int f, void* userdata);
