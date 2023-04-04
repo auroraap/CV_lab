b6d5f18d8c916400762af080787b7572fe91e4fc
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-04-04 09:41:02 +0200

    tuning needed

--- lab4/main_2.cpp
+++ lab4/main_2.cpp
@@ -8,34 +8,36 @@
 using namespace cv;
 
 // Canny
-int T = 0;
-const int T_max = 100;
-const int ratio = 3;
+const int T_low = 50;
+const int T_high = 150;
 const int kernel_size = 3;
 
 // Hough
-const int rho_h = 1;
-const double theta = CV_PI / 180;
-const int thresh = 200;
+const double rho_hough = 1;
+const double theta_hough = CV_PI / 180;
+const int T = 200;
 
 int main ( int argc,char** argv) {
 
 	if (argc < 2 )
 		return 0;
 
-	Mat edges, cdst;
+	Mat edges, cdst, input_image_gray_blurred;
 	std::vector<Vec2f> lines;
 	
 	Mat input_img_gray = imread(argv[1],IMREAD_GRAYSCALE);
 
-	if(input_img_gray.empty())
-	std::cout << "Image not found.\n";
+	if(input_img_gray.empty()){
+		std::cout << "Image not found.\n";
 		return 0;
+	}
+	
+	GaussianBlur(input_img_gray, input_image_gray_blurred, Size(11,11), 0); // Noise reduction
+	Canny(input_image_gray_blurred, edges, T_low, T_high, kernel_size); // Detect edges
+	HoughLines(edges, lines, rho_hough, theta_hough, T); // Detect lines among the detected edges
+	cvtColor(input_img_gray, cdst, COLOR_GRAY2BGR); // Make cdst a 3-channel copy of the input image
 
-	Canny(input_img_gray, edges, 50, 150);
-	HoughLines(edges, lines, rho_h, theta, thresh);
-
-	for( size_t i = 0; i < lines.size(); i++ )
+	for( size_t i = 0; i < lines.size(); i++ ) // Draw lines in cdst
     {
         float rho = lines[i][0], theta = lines[i][1];
         Point pt1, pt2;
@@ -48,8 +50,14 @@ int main ( int argc,char** argv) {
         line( cdst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
     }
 
+	namedWindow("Source");
+	namedWindow("Blurred");
+	namedWindow("Detected Edges");
+	namedWindow("Detected Lines");
 	imshow("Source", input_img_gray);
-    imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+	imshow("Blurred", input_image_gray_blurred);
+	imshow("Detected Edges", edges);
+    imshow("Detected Lines", cdst);
 
 	waitKey(0);
 	return 0;
