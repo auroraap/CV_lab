f46130fa9c0612e42a21d44217b5f196dbac05c9
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-04-16 20:06:44 +0200

    lab5 initial commit

---
+++ lab5/task1.cpp
@@ -0,0 +1,10 @@
+// task1.cpp
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+int main( int argc,char** argv ){
+    return 0;
+}
