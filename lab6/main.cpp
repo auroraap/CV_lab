554754b4b126f02a7b5d77bca7001c0bca163142
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-05-08 20:30:42 +0200

    cleanup

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -44,7 +44,7 @@ int main( int argc, char** argv ){
     sift.setVars(MATCH_THRESHOLD, SIFT_RATIO_THRESH, K, DET_THRESH);
     sift.buildDescriptor(img1, img2, &sift_kp1, &sift_kp2, &sift_des1, &sift_des2);
     vector<DMatch> sift_matches = sift.matchKeypoints(sift_des1, sift_des2);
-    sift.check_similarity(sift_matches, sift_kp1, sift_kp2);
+    sift.checkSimilarity(sift_matches, sift_kp1, sift_kp2);
 
     // ~~~~~~~~~ SURF ~~~~~~~~~
     cout << "SURF\n";
@@ -56,7 +56,7 @@ int main( int argc, char** argv ){
     surf.setVars(MATCH_THRESHOLD, SURF_RATIO_THRESH, K, DET_THRESH);
     surf.buildDescriptor(img1, img2, &surf_kp1, &surf_kp2, &surf_des1, &surf_des2);
     vector<DMatch> surf_matches = surf.matchKeypoints(surf_des1, surf_des2);
-    surf.check_similarity(surf_matches, surf_kp1, surf_kp2);
+    surf.checkSimilarity(surf_matches, surf_kp1, surf_kp2);
 
     // ~~~~~~~~~ ORB ~~~~~~~~~
     cout << "ORB\n";
@@ -68,7 +68,7 @@ int main( int argc, char** argv ){
     orb.setVars(MATCH_THRESHOLD, ORB_RATIO_THRESH, K, DET_THRESH);
     surf.buildDescriptor(img1, img2, &orb_kp1, &orb_kp2, &orb_des1, &orb_des2);
     vector<DMatch> orb_matches = orb.matchKeypoints(orb_des1, orb_des2);
-    orb.check_similarity(orb_matches, orb_kp1, orb_kp2);
+    orb.checkSimilarity(orb_matches, orb_kp1, orb_kp2);
 
     Mat sift_result = sift.drawResult(img1, img2, sift_kp1, sift_kp2, sift_matches);
     Mat surf_result = surf.drawResult(img1, img2, surf_kp1, surf_kp2, surf_matches);
