a4d70d6815dfd4fbfeda3ac3ba9689468f3f0fc8
Aurora Åsgård Pedersen <aurora@Auroras-MacBook-Air.local>
2023-05-08 20:28:35 +0200

    cleanup

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -13,21 +13,29 @@ using namespace cv;
 using namespace std;
 
 const int K = 2;
-const int MATCH_THRESHOLD = 10;
-const float ORB_RATIO_THRESH = 0.6;
-const float SIFT_RATIO_THRESH = 0.6;
-const float SURF_RATIO_THRESH = 0.6;
+const int MATCH_THRESHOLD = 25;
+const float ORB_RATIO_THRESH = 0.8;
+const float SIFT_RATIO_THRESH = 0.8;
+const float SURF_RATIO_THRESH = 0.8;
 const float DET_THRESH = 0.1;
 const int SURF_HESSIAN_THRESH = 400;
 
 int main( int argc, char** argv ){
-    if (argc < 2 )
-		return 0;
+    if (argc < 2 ){
+        cout << "Make sure to include two image names in the command line.\n";
+        return 0;
+    }
+		
 	
 	Mat img1 = imread(argv[1], IMREAD_GRAYSCALE);
     Mat img2 = imread(argv[2], IMREAD_GRAYSCALE);
 
+    if ((img1.empty()) or (img2.empty())){
+        cout << "The provided images were not found.\n";
+        return 0;
+    }
     // ~~~~~~~~~ SIFT ~~~~~~~~~ 
+    cout << "SIFT\n";
     Matcher sift;
     vector< KeyPoint> sift_kp1, sift_kp2;
     Mat sift_des1, sift_des2;
@@ -38,7 +46,8 @@ int main( int argc, char** argv ){
     vector<DMatch> sift_matches = sift.matchKeypoints(sift_des1, sift_des2);
     sift.check_similarity(sift_matches, sift_kp1, sift_kp2);
 
-    // ~~~~~~~~~ SURF ~~~~~~~~~ 
+    // ~~~~~~~~~ SURF ~~~~~~~~~
+    cout << "SURF\n";
     Matcher surf;
     vector< KeyPoint> surf_kp1, surf_kp2;
     Mat surf_des1, surf_des2;
@@ -49,12 +58,13 @@ int main( int argc, char** argv ){
     vector<DMatch> surf_matches = surf.matchKeypoints(surf_des1, surf_des2);
     surf.check_similarity(surf_matches, surf_kp1, surf_kp2);
 
-    // ~~~~~~~~~ ORB ~~~~~~~~~ 
+    // ~~~~~~~~~ ORB ~~~~~~~~~
+    cout << "ORB\n";
     Matcher orb;
     vector< KeyPoint> orb_kp1, orb_kp2;
     Mat orb_des1, orb_des2;
     orb.detector = ORB::create();
-    orb.matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE);
+    orb.matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE_L1);
     orb.setVars(MATCH_THRESHOLD, ORB_RATIO_THRESH, K, DET_THRESH);
     surf.buildDescriptor(img1, img2, &orb_kp1, &orb_kp2, &orb_des1, &orb_des2);
     vector<DMatch> orb_matches = orb.matchKeypoints(orb_des1, orb_des2);
@@ -63,8 +73,11 @@ int main( int argc, char** argv ){
     Mat sift_result = sift.drawResult(img1, img2, sift_kp1, sift_kp2, sift_matches);
     Mat surf_result = surf.drawResult(img1, img2, surf_kp1, surf_kp2, surf_matches);
     Mat orb_result = orb.drawResult(img1, img2, orb_kp1, orb_kp2, orb_matches);
+    imwrite("SIFT.jpg", sift_result);
     imshow("SIFT", sift_result);
+    imwrite("SURF.jpg", surf_result);
     imshow("SURF", surf_result);
+    imwrite("ORB.jpg", orb_result);
     imshow("ORB", orb_result);
     waitKey(0);
     
